b934d4e4a3b69ab5d47ee6f1e5c10b994789c3e2
Carlos Anguiano <c.joseanguiano@gmail.com>
2017-11-02 17:58:49 -0600

    Nueva-Alarma

---
+++ Nueva Alarma/app/src/main/java/com/example/carlosanguiano/nuevaalarma/AlarmReceiver.java
@@ -0,0 +1,40 @@
+package com.example.carlosanguiano.nuevaalarma;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by Carlos Anguiano on 02/11/17.
+ * For more info contact: c.joseanguiano@gmail.com
+ */
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    String TAG = "AlarmReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO Auto-generated method stub
+
+        if (intent.getAction() != null && context != null) {
+            if (intent.getAction().equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED)) {
+                // Set the alarm here.
+                Log.d(TAG, "onReceive: BOOT_COMPLETED");
+                LocalData localData = new LocalData(context);
+                NotificationScheduler.setReminder(context, AlarmReceiver.class, localData.get_hour(), localData.get_min());
+                return;
+            }
+        }
+
+        Log.d(TAG, "onReceive: ");
+
+        //Trigger the notification
+        NotificationScheduler.showNotification(context, MainActivity.class,
+                "You have 5 unwatched videos", "Watch them now?");
+
+    }
+}
+
+
